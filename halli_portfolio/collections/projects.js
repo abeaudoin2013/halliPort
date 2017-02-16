799ec229af1256b727ab4330a3db5500a8a17f77
Andy <abeaudoin2013@gmail.com>
2017-02-16 10:16:43 -0500

    fixed some image stuff. files are deleted on meteor restart or if cache cleared I think. It should be fine once we have it in production

--- halli_portfolio/collections/projects.js
+++ halli_portfolio/collections/projects.js
@@ -1,15 +1,17 @@
 Images = new FilesCollection({
   collectionName: 'Images',
-  allowClientCode: true, // Required to let you remove uploaded file
-  onBeforeUpload: function (file) {
-    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.ext)) {
-      return true;
-    } else {
-      return 'Please upload image, with size equal or less than 10MB';
-    }
-  },
-  storagePath: "./imgs"
+  allowClientCode: true // Required to let you remove uploaded file
+  // onBeforeUpload: function (file) {
+  //   // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+  //   if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.ext)) {
+  //     return true;
+  //   } else {
+  //     return 'Please upload image, with size equal or less than 10MB';
+  //   }
+  // },
+  // storagePath: function () {
+  //   return "./imgs/"
+  // }
 });
 
 Projects = new Meteor.Collection('projects');
